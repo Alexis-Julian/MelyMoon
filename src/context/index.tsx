2d58220b63def8904fa001173e019a4845eae23b
Alexis <alexisjrojas@hotmail.es>
2024-04-06 22:42:44 -0300

    Context provider

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,11 +1,13 @@
 "use client";
 import { createContext, useState, useEffect, useContext } from "react";
 
-const AppContext = createContext();
+const AppContext = createContext({ hello: "word" });
 
 export function AppWrapper({ children }: { children: React.ReactNode }) {
-	let [state, setState] = useState("Hello");
+	let [state, setState] = useState({ hello: "word" });
 	return <AppContext.Provider value={state}>{children}</AppContext.Provider>;
 }
 
-export function useAppContext() {}
+export function useAppContext() {
+	return useContext(AppContext);
+}
